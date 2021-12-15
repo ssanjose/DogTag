4129df888c78dcf364d443103dc5b92f896e65ec
ssanjose <ssanjose@academic.rrc.ca>
2021-12-15 16:40:45 -0600

    Added navbar and footer partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: "main#index"
+  get "about", to: "main#about"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
