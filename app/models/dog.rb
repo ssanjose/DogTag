7e1f237944532000e6cd5d4c906aa718b38a6acc
ssanjose <ssanjose@academic.rrc.ca>
2021-12-15 22:14:14 -0600

    Added active storage to store dog pictures

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,8 @@ class Dog < ApplicationRecord
   belongs_to :owner
   has_many :breed_dogs
 
+  has_one_attached :image
+
   validates :name, :age, :size, presence: true
   validates :name, uniqueness: true
   validates :likes, numericality: true
