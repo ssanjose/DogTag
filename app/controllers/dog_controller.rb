51781fab9fed75b54c31830ca1b1e7e0c1a0bfef
ssanjose <ssanjose@academic.rrc.ca>
2021-12-17 09:55:19 -0600

    Added search function to project

--- app/controllers/dog_controller.rb
+++ app/controllers/dog_controller.rb
@@ -5,7 +5,30 @@ class DogController < ApplicationController
     @dog = Dog.find(params[:id])
   end
 
-  def search; end
+  def search
+    if (params[:search] != "") && params[:breed_id].to_i != 0
+      breed_id = Breed.find(params[:breed_id].to_i).id
+      # @dogs = Dog.where("lower(name) LIKE :search",
+      #                   search: "%#{params[:search]}%").map do |x|
+      #   if x.breed_dogs.index do |y|
+      #        y.breed_id == breed_id
+      #      end.nil?
+      #     return x
+      #   end
+      # end
+      @dogs = Dog.joins(:breed_dogs)
+                 .where("lower(name) LIKE :search",
+                        search: "%#{params[:search]}%")
+                 .where('breed_dogs.breed_id': breed_id)
+    elsif (params[:search] != "") && params[:breed_id].to_i == 0
+      @dogs = Dog.where("lower(name) LIKE :search",
+                        search: "%#{params[:search]}%")
+    elsif params[:breed_id].to_i == 0
+      @dogs = Dog.all
+    else
+      @dogs = Breed.find(params[:breed_id]).breed_dogs.map { |x| x.dog }
+    end
+  end
 
   def upvote
     dog = Dog.find(params[:id])
